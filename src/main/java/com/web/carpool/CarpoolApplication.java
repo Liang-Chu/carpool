dbc1a689442d509ac9260ab3c5785b3f3aef995a
bumblebees1111 <gu000031@algonquinlive.com>
2019-01-26 18:27:05 -0500

    Starter file for the carpool project

---
+++ src/main/java/com/web/carpool/CarpoolApplication.java
@@ -0,0 +1,14 @@
+package com.web.carpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/web/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(path="/")
+    public ModelAndView showSignUp(ModelAndView modelAndView) {
+		modelAndView.setViewName("index");
+		return modelAndView;
+    }
+}
